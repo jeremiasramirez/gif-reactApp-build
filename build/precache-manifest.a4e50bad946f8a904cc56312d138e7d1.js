a740124e9e6058ac9266ed4ae7039a01c4685ed7
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-29 12:25:03 -0400

    creando build de app

---
+++ build/precache-manifest.a4e50bad946f8a904cc56312d138e7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3500ae99ec6732e2a2a1111124ab8f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "041c81933bb6dcba8e62",
+    "url": "/static/css/main.d4993ee5.chunk.css"
+  },
+  {
+    "revision": "6e5525b9b8c1bf189b8e",
+    "url": "/static/js/2.01cd59d7.chunk.js"
+  },
+  {
+    "revision": "028d1b8f21ffff8caf0c799a051bd2b8",
+    "url": "/static/js/2.01cd59d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041c81933bb6dcba8e62",
+    "url": "/static/js/main.10fbaedd.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
